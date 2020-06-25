d01a58ff8f88de31dee9dbb7efc264afd6f5bc3c
QuintenDes <quintendeswaef@gmail.com>
2020-06-25 15:45:05 +0200

    Configprosp for secrets

---
+++ spring-cloud-karmahostage-secrets/src/main/java/com/karmahostage/cloud/SecretsConfigProperties.java
@@ -0,0 +1,20 @@
+package com.karmahostage.cloud;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@ConfigurationProperties("spring.cloud.karmahostage.secrets")
+public class SecretsConfigProperties {
+
+    private List<String> paths = new LinkedList<>();
+
+    public List<String> getPaths() {
+        return this.paths;
+    }
+
+    public void setPaths(List<String> paths) {
+        this.paths = paths;
+    }
+}
